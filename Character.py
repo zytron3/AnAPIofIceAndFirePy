9c7e1a1a8fe9ed541d7640fa2da5b56a51dc4319
Prater <Grant.Prater@adph.state.al.us>
2022-02-07 15:02:22 -0600

    Added characters

---
+++ Character.py
@@ -0,0 +1,20 @@
+class Character(object):
+    def __init__(self, url, name, gender, culture, born, died, titles, aliases, \
+    father, mother, spouse, allegiances, \
+    books, povBooks, tvSeries, playedBy):
+        self.url = url
+        self.name = name
+        self.gender = gender
+        self.culture = culture
+        self.born = born
+        self.died = died
+        self.titles = titles
+        self.aliases = aliases
+        self.father = father
+        self.mother = mother
+        self.spouse = spouse
+        self.allegiances = allegiances
+        self.books = books
+        self.povBooks = povBooks
+        self.tvSeries = tvSeries
+        self.playedBy = playedBy
--- main.py
+++ main.py
@@ -1,31 +1,36 @@
 import requests
-import Book
+from Book import Book
+from Character import Character
 
-def get_books(number=None, name=None, fromReleaseDate=None, toReleaseDate=None):
-    url = "https://www.anapioficeandfire.com/api/books/"
-    if (number is not None):
-        url += str(number) + "/"
-    if name is None and fromReleaseDate is None and toReleaseDate is None:
-        res = requests.get(url).json()
-        if type(res) == dict:
-            res = [res]
-        return [Book.Book(*x.values()) for x in res]
-    else:
-        url += "?"
-    if name is not None:
-        url += "name=" + name + "&"
-    if fromReleaseDate is not None:
-        url += "fromReleaseDate=" + fromReleaseDate + "&"
-    if toReleaseDate is not None:
-        url += "toReleaseDate=" + toReleaseDate + "&"
-    url = url[:-1]  
-    
-    res = requests.get(url).json()
-    #only 1 book
-    if (type(res) == dict):
-        return [Book.Book(*res.values())]
-    else:
-        return [Book.Book(*(x.values())) for x in res]
+def get_json(**params):
+    url = "https://www.anapioficeandfire.com/api/" + params["type"] + "/"
+    if params["id"] is not None:
+        url += str(params["id"])
+    url += "/?"
+    del params["type"]
+    del params["id"]
+    for param in params.items():
+        url += str(param[0]) + "=" + str(param[1]) + "&"
+    url = url[:-1]
+    return requests.get(url).json()
 
-print(get_books(number=2)[0].authors)
+def get_books(id=None, name=None, fromReleaseDate=None, toReleaseDate=None):
+    args = {key: value for (key, value) in locals().items() if value is not None}
+    args["type"] = "books"
+    json = get_json(**args)
+    if (type(json) == dict):
+        return [Book(*json.values())]
+    else:
+        return [Book(*(x.values())) for x in json]
+
+def get_characters(id=None, name=None, gender=None, culture=None, born=None, died=None, isAlive=None):
+    args = {key: value for (key, value) in locals().items() if value is not None}
+    args["type"] = "characters"
+    json = get_json(**args)
+    if (type(json) == dict):
+        return [Character(*json.values())]
+    else:
+        return [Character(*(x.values())) for x in json]
+
+print(get_characters(id=2)[0].name)
 #print(get_books()[0].mediaType)
